c2c852fbea5944f67ec0f5aeb4842fec171c8320
Alvin Ng <alvin.cl.ng@gmail.com>
2024-04-13 09:05:36 -0700

    Updated instructions and added axios

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
       <h1>1 - First thing is to add the Navbar to EVERYPAGE</h1>
       <h1>2 - Add all the 2 pages on top right of each Navbar</h1>
       <h1>3 - In the Counter page we will build a counter</h1>
-      <h1>4 - In the About page we will build a one line form to make an api call to post "/api/about"</h1>
+      <h1>4 - In the About page we will build a form to make an api call to post "/api/post"</h1>
     </main>
   );
 }
